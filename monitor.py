c590476dc7638d9f1fa2b0eb87661f3a197bcfa4
shobhit <shobjobs@gmail.com>
2018-12-10 01:12:04 +0530

    first checkin

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from scapy.all import *
+import pymysql
 
 def monitor_callback(pkt):
     if not pkt.haslayer(Dot11): # Discard frames with no 802.11 headers
@@ -7,5 +8,8 @@ def monitor_callback(pkt):
     if pkt.type == 1: # Discard control frames
         return
     print pkt.time, pkt[Dot11].addr2
+    con = pymysql.connect('192.168.31.120', 'test', 'password', 'users')
+    con.execute("""INSERT INTO orders (connection, duration, freqconnection) VALUES ("%s", "%s", "%s")""" 
+    con.commit()
 
 sniff(prn=monitor_callback, store=0)
